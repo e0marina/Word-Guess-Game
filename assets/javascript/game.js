0c064c0fa13837dc747e890cc0705fe36793e41a
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-12 11:09:49 -0800

    updated psuedo code in JS file

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//create something to hold all of the words that could be guessed.
+
+// make it so computer chooses a word at random, before the user chooses any letters
+
+//Press a key to get started - keyup, this key shouldn't log as a letter guessed.
+
+//under current word the number of letters shown as dashes and dashes fill in with
+//their respective letters as they are guessed by user --how to do this?
+
+//When a user guesses a letter
+//either it is correct (true) and the letter is revealed in it's spot.
+//or it is false and displays in the letters already guessed area, in uppercase.
+//the number of guesses count decreases by 1
+
+//if the user guesses the word they win, and win increases by 1
+
+//if the user runs out of guesses:
+//new word is displayed
+//number of guesses returns to 15
+//letters already guessed resets

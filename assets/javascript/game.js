a252e7e1fceb8b17775a8e5bc18050fa9acf4e59
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-12 22:03:52 -0800

    added a function can pass multi arrays through

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,23 @@
+//whatever key pressed should convert to uppercase
+
+//either it is correct (true) and the letter is revealed in it's spot.
+//or it is false and displays in the letters already guessed area, in uppercase.
+//the number of guesses count decreases by 1
+
+//if the user guesses the word they win, and win increases by 1
+
+//if the user runs out of guesses:
+//new word is displayed
+//number of guesses returns to 15
+//letters already guessed resets
+
+//display with textContent
+
 //create arrays to hold the letters inside the words that could be guessed.
 kiwiArr = ["k", "i", "w", "i"];
 appleArr = ["a", "p", "p", "l", "e"];
 pearArr = ["p", "e", "a", "r"];
 bananaArr = ["b", "a", "n", "a", "n", "a", "s"];
-
-//not sure that computer needs to even make a choice
-//coputerWordChoice = ["kiwi", "apple", "pear", "banana"];
 
 //VARIABLES
 //===============================================================================
@@ -26,42 +38,30 @@ var lettersAlreadyGuessedText = document.getElementById(
 // FUNCTIONS
 // ==============================================================================
 
-// Computer chooses a word at random, before the user chooses any letters
-// var computerRandomChoice =
-//   coputerWordChoice[Math.floor(Math.random() * coputerWordChoice.length)];
+//
+//When a user types a letter to guess a letter
+document.onkeyup = function(event) {
+  //Determines which key was pressed.
+  var userGuess = event.key;
+  console.log(userGuess);
 
-// // Computer matches computerRandomChoice with the spelled-out-letter fruit name's 1st letter
+  //Define a function that we can pass through multiple arrays
+  function arrayMatch(arr) {
+    //We then loop through the selected array
+    for (i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+      if (userGuess === arr[i]) {
+      }
+    }
+    console.log("__________");
+  }
+  //under current word the number of letters shown as dashes and dashes fill in with
+  //their respective letters as they are guessed by user
 
-// if (kiwiArr || pearArr || bananaArr || appleArr === computerRandomChoice) {
-//   console.log("true");
-//   console.log(computerRandomChoice);
-// }
-
-//Press a key to start the game
-
-//under current word the number of letters shown as dashes and dashes fill in with
-//their respective letters as they are guessed by user
-
-//When a user guesses a letter
-//whatever key pressed should convert to uppercase
-
-//either it is correct (true) and the letter is revealed in it's spot.
-//or it is false and displays in the letters already guessed area, in uppercase.
-//the number of guesses count decreases by 1
-
-//if the user guesses the word they win, and win increases by 1
-
-//if the user runs out of guesses:
-//new word is displayed
-//number of guesses returns to 15
-//letters already guessed resets
-
-//display with textContent
-
-//IDEAS
-//============================
-//use empty array and push user guesses into it? then compare user guess array with computer choices?
-//create a func that can handle all of the arrays
-//use indexOf to find out index of an array and compare it to user's guess
-//does it make sense to use an object, for instance a dash and then replaced by the guessed letter
-//should be able to write something that uses length to "count" how many letters in a array and display that many dashes
+  //FUNCTION CALLS
+  //===============================
+  arrayMatch(kiwiArr);
+  arrayMatch(appleArr);
+  arrayMatch(pearArr);
+  arrayMatch(bananaArr);
+};

0f7c43ac603a927ce9265dc9d054e24e3b582621
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-12 20:08:31 -0800

    linked js and jquery to html file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,15 @@
-//create something to hold all of the words that could be guessed.
-computerChoice = [
-  "FRIDA KAHLO",
-  "LEONARDO DAVINCI",
-  "GEORGIA OKEEFFE",
-  "ANDY WARHOL",
-  "JEFF KOONS",
-  "EDGAR DEGAS",
-  "CLAUDE MONET",
-  "VINCENT VAN GOGH",
-  "YAYOI KUSAMA",
-  "ALICE NEEL",
-  "MARY CASSATT",
-  "JENNY SAVILLE"
-];
+//create arrays to hold the letters inside the words that could be guessed.
+kiwiArr = ["k", "i", "w", "i"];
+kiwiDashArr = ["_", "_", "_", "_"];
+//appleArr = ["a", "p", "p", "l", "e"];
+//pearArr = ["p", "e", "a", "r"];
+
+//IDEAS
+//============================
+//use empty array and push user guesses into it? then compare user guess array with computer choices?
+//create a func that can handle all of the arrays
+//use indexOf to find out index of an array and compare it to user's guess
+//does it make sense to use an object, for instance a dash and then replaced by the guessed letter
 //VARIABLES
 //===============================================================================
 
@@ -33,15 +30,17 @@ var lettersAlreadyGuessedText = document.getElementById(
 // ==============================================================================
 
 // make it so computer chooses a word from array at random, before the user chooses any letters
-var computerGuess =
-  computerChoice[Math.floor(Math.random() * computerChoice.length)];
-//Press a key to get started - keyup, this key shouldn't log as a letter guessed.
+//var computerGuess =
+//computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+//Press a key to start the game
 
 //under current word the number of letters shown as dashes and dashes fill in with
-//their respective letters as they are guessed by user --how to do this?
+//their respective letters as they are guessed by user
 
 //When a user guesses a letter
 //whatever key pressed should convert to uppercase
+
 //either it is correct (true) and the letter is revealed in it's spot.
 //or it is false and displays in the letters already guessed area, in uppercase.
 //the number of guesses count decreases by 1
@@ -52,3 +51,5 @@ var computerGuess =
 //new word is displayed
 //number of guesses returns to 15
 //letters already guessed resets
+
+//display with textContent

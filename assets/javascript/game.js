e56731243d20ef500228995666b269f34078fad7
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-15 20:42:56 -0800

    wrote on paper, updated some variables and funcs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
 //NOTES
 //==================
 
-//whatever key pressed should convert to uppercase
-
 //either it is correct (true) and the letter is revealed in it's spot.
 //or it is false and displays in the letters already guessed area, in uppercase.
 //the number of guesses count decreases by 1
@@ -41,35 +39,152 @@ var lettersAlreadyGuessedText = document.getElementById(
 
 // FUNCTIONS
 // ==============================================================================
-//Random fruit chosen by the computer
+//listens for alpha key presses, since we are only interested in alpha characters
+document.addEventListener("keypress", function(event) {
+  switch (event.keyCode) {
+    case 97:
+      console.log("a");
+      keyPressFunction("a");
+      break;
+    case 98:
+      console.log("b");
+      keyPressFunction("b");
+      break;
+    case 99:
+      console.log("c");
+      keyPressFunction("c");
+      break;
+    case 100:
+      console.log("d");
+      keyPressFunction("d");
+      break;
+    case 101:
+      console.log("e");
+      keyPressFunction("e");
+      break;
+    case 102:
+      console.log("f");
+      keyPressFunction("f");
+      break;
+    case 103:
+      console.log("g");
+      keyPressFunction("g");
+      break;
+    case 104:
+      console.log("h");
+      keyPressFunction("h");
+      break;
+    case 105:
+      console.log("i");
+      keyPressFunction("i");
+      break;
+    case 106:
+      console.log("j");
+      keyPressFunction("j");
+      break;
+    case 107:
+      console.log("k");
+      keyPressFunction("k");
+      break;
+    case 108:
+      console.log("l");
+      keyPressFunction("l");
+      break;
+    case 109:
+      console.log("m");
+      keyPressFunction("m");
+      break;
+    case 110:
+      console.log("n");
+      keyPressFunction("n");
+      break;
+    case 111:
+      console.log("o");
+      keyPressFunction("o");
+      break;
+    case 112:
+      console.log("p");
+      keyPressFunction("p");
+      break;
+    case 113:
+      console.log("q");
+      keyPressFunction("q");
+      break;
+    case 114:
+      console.log("r");
+      keyPressFunction("r");
+      break;
+    case 115:
+      console.log("s");
+      keyPressFunction("s");
+      break;
+    case 116:
+      console.log("t");
+      keyPressFunction("t");
+      break;
+    case 117:
+      console.log("u");
+      keyPressFunction("u");
+      break;
+    case 118:
+      console.log("v");
+      keyPressFunction("v");
+      break;
+    case 119:
+      console.log("w");
+      keyPressFunction("w");
+      break;
+    case 120:
+      console.log("x");
+      keyPressFunction("x");
+      break;
+    case 121:
+      console.log("y");
+      keyPressFunction("y");
+      break;
+    case 122:
+      console.log("z");
+      keyPressFunction("z");
+      break;
 
+    default:
+      break;
+  }
+});
+
+//Random fruit chosen by the computer
 var word = fruits[Math.floor(Math.random() * fruits.length)];
+//test that the computer is choosing a word from the array
+console.log(word);
+//note letters left for filling-in when user guesses the word
+var lettersLeft = word.length;
 
 //MAIN
 //======================================================================
-
-//Game
-document.onkeyup = function(event) {
-  var userGuess = event.key;
-  var dashesArr = [];
-  for (i = 0; i < word.length; i++) {
-    dashesArr[i] = "_";
-    //show the dashes on the page
-    document.getElementById("current-word").innerHTML = dashesArr.join("");
+//dashes for the length of the word...
+var dashesArr = [];
+for (i = 0; i < word.length; i++) {
+  dashesArr[i] = "_";
+  //show the dashes on the page
+  document.getElementById("dashes-section").innerHTML = dashesArr;
+}
+//GAME
+//
+function keyPressFunction(userGuess) {
+  if (userGuess === word) {
+    wins++;
+  } else {
+    triesLeft--;
+    document.getElementById("letters-already-guessed").innerHTML += userGuess;
   }
-  var lettersLeft = word.length;
-  //while (lettersLeft > 0) {
+}
 
-  //update page with progress
-  // document.getElementById("current-word").innerHTML = dashesArr;
+//Display the wins, losses, guesses left, list guesses so far
+winsText.textContent = "wins: " + wins;
+numTriesLeftText.textContent = "num-tries-left: " + triesLeft;
 
-  //Update the game with the user's guess
-  //for loop sometimes a solution to a issue that's not there
-  //write out on paper (like what happens on key up)
-  for (j = 0; j < word.length; j++) {
-    if (word[j] === userGuess) {
-      dashesArr[j] = userGuess;
-      lettersLeft--;
-    }
-  }
-};
+// //Update the game with the user's guess
+// if (lettersLeft === userGuess) {
+//   dashesArr = userGuess;
+//   console.log(userGuess);
+// }
